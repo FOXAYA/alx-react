273924599a4a1a1ab8afc63ecbeb45eebb6e92fb
FOXAYA <omniasaged111@gmail.com>
2024-11-12 14:39:16 +0200

    updated

--- 0x04-React_inline_styling/task_0/dashboard/src/Notifications/NotificationItemShape.js
+++
@@ -1,8 +0,0 @@
-import PropTypes from 'prop-types';
-
-export default PropTypes.shape({
-    id: PropTypes.number.isRequired,
-    type: PropTypes.string.isRequired,
-    value: PropTypes.string,
-    html: PropTypes.shape({__html: PropTypes.string})
-});
